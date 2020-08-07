5b9f2bc54985023d612cd391ba877c19ebb17b64
Phạm Lê Song Tuấn <69331470+tuan115@users.noreply.github.com>
2020-08-07 10:49:53 +0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -10,14 +10,50 @@ Ex:
 #include <stdlib.h>
 
 int ex3(char *str){
-	//your codes here
+	char string[100], word[20], max[20], min[20], c;
+	int i = 0, j = 0, flag = 0;
+    do
+    {
+        fflush(stdin);
+        c = getchar();
+        string[i++] = c;
+
+    } while (c != '\n');
+    string[i - 1] = '\0';
+    for (i = 0; i < strlen(string); i++)
+    {
+        while (i < strlen(string) && !isspace(string[i]) && isalnum(string[i]))
+        {
+            word[j++] = string[i++];
+        }
+        if (j != 0)
+        {
+            word[j] = '\0';
+            if (!flag)
+            {
+                flag = !flag;
+                strcpy(max, word);
+                strcpy(min, word);
+            }
+            if (strlen(word) > strlen(max))
+            {
+                strcpy(max, word);
+            }
+            if (strlen(word) < strlen(min))
+            {
+                strcpy(min, word);
+            }
+            j = 0;
+        }
+    }
+}
 }
 
 int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	char *str = "This is a string";
-
+	char max[20], min[20], string[20];
+	char* str = "This is a string";
+        printf("Enter string: ");
+        printf("The largest word is '%s' and smallest word is '%s' in '%s'.\n", max, min, string);
 	ex3(str);
-		
 	return 0;
 }
