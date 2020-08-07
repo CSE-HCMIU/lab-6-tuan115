c973e76893146f399dc1ee5fa6851815f2b7f427
Phạm Lê Song Tuấn <69331470+tuan115@users.noreply.github.com>
2020-08-07 09:22:52 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -7,16 +7,70 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int ex1(int testcase){
-	//your codes here
-}
+void convert(char* num) {
+    int len = strlen(num);
 
-int main(int argc, char *argv[]) {
-	int testcase = atoi(argv[1]);
-	
-	ex1(testcase);
-		
-	return 0;
+    if (len == 0) {
+        printf(stderr, "empty string\n");
+        return;
+    }
+    if (len > 4) {
+        printf(stderr, "Length more than 4 is not supported\n");
+        return;
+    }
+  
+    char* single_digit[] = { "zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine" };
+
+    char* tens_place[] = { "", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+    char* tens_multiple[] = { "", "", "twenty", "thirty", "forty", "fifty","sixty", "seventy", "eighty", "ninety" };
+    char* tens_power[] = { "hundred", "thousand" };
+  
+    printf("\n%s: ", num);
+
+
+    if (len == 1) {
+        printf("%s\n", single_digit[*num - '0']);
+        return;
+    }
+ 
+    while (*num != '\0') {
+ 
+        if (len >= 3) {
+            if (*num - '0' != 0) {
+                printf("%s ", single_digit[*num - '0']);
+                printf("%s ", tens_power[len - 3]); 
+            }
+            --len;
+        }
+ 
+        else {
+
+            if (*num == '1') {
+                int sum = *num - '0' + *(num + 1) - '0';
+                printf("%s\n", tens_place[sum]);
+                return;
+            }
+
+            else if (*num == '2' && *(num + 1) == '0') {
+                printf("twenty\n");
+                return;
+            }
+            else {
+                int i = *num - '0';
+                printf("%s ", i ? tens_multiple[i] : "");
+                ++num;
+                if (*num != '0')
+                    printf("%s ", single_digit[*num - '0']);
+            }
+        }
+        ++num;
+    }
+}
+int main() {
+    convert("9132");
+    return 0;
 }
